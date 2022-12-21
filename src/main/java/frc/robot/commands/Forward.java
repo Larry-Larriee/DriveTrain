0a887759f03a04aa982afda87eec1379734c8627
Larry Le <lle114@student.hbuhsd.edu>
2022-12-20 17:09:05 -0800

    Minor Changes

--- src/main/java/frc/robot/commands/Forward.java
+++ src/main/java/frc/robot/commands/Forward.java
@@ -8,36 +8,56 @@ import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
+public class Forward extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final Drivebase m_drivebase;
+  private double distance = 0;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public Forward(Drivebase drivebase, double inches) {
+    m_drivebase = drivebase;
+    distance = inches;
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+    m_drivebase.resetEncoder();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    m_drivebase.arcadeDrive(0.5,0);
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+    m_drivebase.arcadeDrive(0.0,0);
+    
+
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+
+    if (m_drivebase.getLeftDistanceInch() >= distance){
+      return true;
+    }
+
     return false;
   }
 }
--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -3,16 +3,57 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.SparkMaxAlternateEncoder;
 
-public class ExampleSubsystem extends SubsystemBase {
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
+
+public class Drivebase extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+
+  CANSparkMax m_leftMotor = new CANSparkMax(0, MotorType.kBrushless);
+  CANSparkMax m_rightMotor = new CANSparkMax(0, MotorType.kBrushless);
+
+  SparkMaxAlternateEncoder m_leftEncoder = m_leftMotor.getAlternativeEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 20);
+  SparkMaxAlternateEncoder m_leftEncoder = m_rightMotor.getAlternativeEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 20);
+
+  DifferentialDrive m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+  private double diameter = 500;
+
+  public void resetEncoder(){
+    m_leftEncoder.setPosition(0);
+    m_rightEncoder.setPosition(0);
+
+  }
+  
+  public void getLeftDistanceInch(){
+    m_leftEncoder.getPosition() * diameter * Math.PI;
+
+  }
+
+  public void getRightDistanceInch(){
+    m_rightEncoder.getPosition() * diameter * Math.PI;
+
+  }
+
+
+  public void arcadeDrive(double forwardSpeed, double turnSpeed){
+    m_drive.arcadeDrive(forwardSpeed, turnSpeed);
+  }
+
+  public Drivebase() {
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
   }
 
   @Override
